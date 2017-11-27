73f0e520f401f99a0bdd9f20df37c163bb8f6c9d
jakoblorz <jakob@lorz.org>
2017-11-27 12:34:55 +0100

    created project, added argument parsing

---
+++ index.ts
@@ -0,0 +1,69 @@
+#! /usr/bin/env node
+
+/**
+ * MIT License
+ *
+ * Copyright (c) 2017 Jakob Lorz
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ */
+
+import * as cluster from "cluster";
+import * as os from "os";
+import * as path from "path";
+
+type SupportedArgCommands = "-c" | "-p" | "-f" | "-v";
+
+interface IPreMapArg {
+    type: "path" | "arg-command" | "arg-value";
+    payload: any;
+}
+
+interface IArgCommand {
+    type: SupportedArgCommands;
+    payload: string | number;
+}
+
+const args: IPreMapArg[] = process.argv
+    .filter((arg, i) => i > 1)
+    .map<IPreMapArg>((arg) => arg.charAt(0) === "." || arg.charAt(0) === "/" ?
+        ({ type: "path", payload: arg }) : (arg.charAt(0) === "-" ?
+            ({ type: "arg-command", payload: arg }) : ({ type: "arg-value", payload: arg })));
+
+const buildArgumentList = (lst: IArgCommand[], curr: IPreMapArg) => {
+    if (curr.type === "arg-value" && lst[lst.length - 1] !== undefined) {
+        lst[lst.length - 1].payload = curr.payload;
+    }
+
+    if (curr.type === "arg-command" && ["-c", "-p", "-f", "-v"].indexOf(curr.payload) !== -1) {
+        lst.push({ type: curr.payload as SupportedArgCommands, payload: "" });
+    }
+
+    return lst;
+};
+
+const commands: IArgCommand[] = args
+    .filter((arg) => arg.type !== "path")
+    .reduce(buildArgumentList, [] as IArgCommand[]);
+
+// tslint:disable-next-line:no-console
+console.log(args);
+// tslint:disable-next-line:no-console
+console.log(commands);
